92d99e76d69fde4dd9a0dd7e1dfcc4e8986e9692
lessizoli <lessizoli@gmail.com>
2026-07-20 11:53:35 +0200

    Expose project files from the CRM

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import AuthGate from "../components/AuthGate";
+import ProjectFilesShortcut from "../components/ProjectFilesShortcut";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -29,7 +30,10 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <AuthGate>{children}</AuthGate>
+        <AuthGate>
+          {children}
+          <ProjectFilesShortcut />
+        </AuthGate>
       </body>
     </html>
   );
